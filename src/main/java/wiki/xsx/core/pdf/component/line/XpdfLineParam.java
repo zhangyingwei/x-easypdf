c75e63a64546a835bad075c45ac72075cc865c55
xsx344646090@vip.qq.com <xsx646090xsx>
2020-03-26 11:36:57 +0800

    更新说明

--- src/main/java/wiki/xsx/core/pdf/component/line/XpdfLineParam.java
+++ src/main/java/wiki/xsx/core/pdf/component/line/XpdfLineParam.java
@@ -9,6 +9,17 @@ import org.apache.pdfbox.pdmodel.font.PDFont;
  * @author xsx
  * @date 2020/3/11
  * @since 1.8
+ * <p>
+ * Copyright (c) 2020 xsx All Rights Reserved.
+ * xpdf is licensed under the Mulan PSL v1.
+ * You can use this software according to the terms and conditions of the Mulan PSL v1.
+ * You may obtain a copy of Mulan PSL v1 at:
+ * http://license.coscl.org.cn/MulanPSL
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ * PURPOSE.
+ * See the Mulan PSL v1 for more details.
+ * </p>
  */
 @Data
 @Accessors(chain = true)
--- src/main/java/wiki/xsx/core/pdf/util/PdfUtil.java
+++ src/main/java/wiki/xsx/core/pdf/util/PdfUtil.java
@@ -20,6 +20,17 @@ import java.nio.file.Paths;
  * @author xsx
  * @date 2019/12/19
  * @since 1.8
+ * <p>
+ * Copyright (c) 2020 xsx All Rights Reserved.
+ * xpdf is licensed under the Mulan PSL v1.
+ * You can use this software according to the terms and conditions of the Mulan PSL v1.
+ * You may obtain a copy of Mulan PSL v1 at:
+ * http://license.coscl.org.cn/MulanPSL
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+ * PURPOSE.
+ * See the Mulan PSL v1 for more details.
+ * </p>
  */
 public class PdfUtil {
 
