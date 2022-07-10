0da185e8dcc209cdd902b135a169fe9decdee5b0
xsx <xsx3446460090@vip.qq.com>
2022-07-10 20:01:37 +0800

    - 新增设置渲染类型的方法 - 新增开启灰度的方法 - 新增开启透明的方法 - 新增开启水平合并的方法 - 新增开启垂直合并的方法

--- src/main/java/wiki/xsx/core/pdf/doc/XEasyPdfDocumentImager.java
+++ src/main/java/wiki/xsx/core/pdf/doc/XEasyPdfDocumentImager.java
@@ -2,9 +2,12 @@ package wiki.xsx.core.pdf.doc;
 
 import lombok.SneakyThrows;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
+import org.apache.pdfbox.rendering.RenderDestination;
 import wiki.xsx.core.pdf.component.image.XEasyPdfImageType;
 import wiki.xsx.core.pdf.util.XEasyPdfFileUtil;
+import wiki.xsx.core.pdf.util.XEasyPdfImageUtil;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -14,6 +17,8 @@ import java.io.OutputStream;
 import java.io.Serializable;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * pdf文档图像器
@@ -49,6 +54,26 @@ public class XEasyPdfDocumentImager implements Serializable {
      * 图像DPI
      */
     private Float dpi = 72F;
+    /**
+     * 渲染类型
+     */
+    private RenderType renderType = RenderType.EXPORT;
+    /**
+     * 是否灰度
+     */
+    private Boolean isGray = Boolean.FALSE;
+    /**
+     * 是否透明
+     */
+    private Boolean isAlpha = Boolean.FALSE;
+    /**
+     * 是否合并
+     */
+    private Boolean isMerge = Boolean.FALSE;
+    /**
+     * 是否水平合并
+     */
+    private Boolean isHorizontalMerge = Boolean.FALSE;
 
     /**
      * 构造方法
@@ -68,6 +93,59 @@ public class XEasyPdfDocumentImager implements Serializable {
      */
     public XEasyPdfDocumentImager setDpi(float dpi) {
         this.dpi = Math.abs(dpi);
+        return this;
+    }
+
+    /**
+     * 设置渲染类型
+     *
+     * @param renderType 渲染类型
+     * @return 返回pdf文档图像器
+     */
+    public XEasyPdfDocumentImager setRenderType(RenderType renderType) {
+        this.renderType = renderType;
+        return this;
+    }
+
+    /**
+     * 开启灰度
+     *
+     * @return 返回pdf文档图像器
+     */
+    public XEasyPdfDocumentImager enableGray() {
+        this.isGray = Boolean.TRUE;
+        return this;
+    }
+
+    /**
+     * 开启透明
+     *
+     * @return 返回pdf文档图像器
+     */
+    public XEasyPdfDocumentImager enableAlpha() {
+        this.isAlpha = Boolean.TRUE;
+        return this;
+    }
+
+    /**
+     * 开启水平合并
+     *
+     * @return 返回pdf文档图像器
+     */
+    public XEasyPdfDocumentImager enableHorizontalMerge() {
+        this.isHorizontalMerge = Boolean.TRUE;
+        this.isMerge = Boolean.TRUE;
+        return this;
+    }
+
+    /**
+     * 开启垂直合并
+     *
+     * @return 返回pdf文档图像器
+     */
+    public XEasyPdfDocumentImager enableVerticalMerge() {
+        this.isHorizontalMerge = Boolean.FALSE;
+        this.isMerge = Boolean.TRUE;
         return this;
     }
 
@@ -101,24 +179,46 @@ public class XEasyPdfDocumentImager implements Serializable {
         }
         // 图片格式名称
         String imageTypeName = imageType.name().toLowerCase();
-        // 文件名称构造器
-        StringBuilder fileNameBuilder;
         // 初始化pdfBox文档渲染器
         PDFRenderer renderer = new PDFRenderer(this.document);
+        // 设置渲染目的
+        renderer.setDefaultDestination(this.renderType.destination);
         // 任务文档页面总数
         int pageCount = this.document.getNumberOfPages();
-        // 遍历文档页面
-        for (int i = 0; i < pageCount; i++) {
-            // 新建文件名称构造器
-            fileNameBuilder = new StringBuilder();
-            // 构建文件名称
-            fileNameBuilder.append(outputPath).append(File.separator).append(prefix).append(i + 1).append('.').append(imageTypeName);
+        // 如果合并图片，则合并后保存图片
+        if (this.isMerge) {
+            // 定义图片列表
+            List<BufferedImage> imageList = new ArrayList<>(pageCount);
+            // 遍历页面索引
+            for (int i = 0; i < pageCount; i++) {
+                // 添加图片列表
+                imageList.add(renderer.renderImageWithDPI(i, this.dpi, this.getColorType()));
+            }
+            // 拼接图片
+            BufferedImage bufferedImage = XEasyPdfImageUtil.join(imageList, this.isHorizontalMerge);
             // 获取输出流
-            try (OutputStream outputStream = new BufferedOutputStream(Files.newOutputStream(XEasyPdfFileUtil.createDirectories(Paths.get(fileNameBuilder.toString()))))) {
-                // 渲染图片
-                BufferedImage bufferedImage = renderer.renderImageWithDPI(i, this.dpi);
+            try (OutputStream outputStream = new BufferedOutputStream(Files.newOutputStream(XEasyPdfFileUtil.createDirectories(Paths.get(outputPath + File.separator + prefix + '.' + imageTypeName))))) {
                 // 写出图片
                 ImageIO.write(bufferedImage, imageTypeName, outputStream);
+            }
+        }
+        // 否则单页保存图片
+        else {
+            // 文件名称构造器
+            StringBuilder fileNameBuilder;
+            // 遍历页面索引
+            for (int i = 0; i < pageCount; i++) {
+                // 新建文件名称构造器
+                fileNameBuilder = new StringBuilder();
+                // 构建文件名称
+                fileNameBuilder.append(outputPath).append(File.separator).append(prefix).append(i + 1).append('.').append(imageTypeName);
+                // 获取输出流
+                try (OutputStream outputStream = new BufferedOutputStream(Files.newOutputStream(XEasyPdfFileUtil.createDirectories(Paths.get(fileNameBuilder.toString()))))) {
+                    // 渲染图片
+                    BufferedImage bufferedImage = renderer.renderImageWithDPI(i, this.dpi, this.getColorType());
+                    // 写出图片
+                    ImageIO.write(bufferedImage, imageTypeName, outputStream);
+                }
             }
         }
         return this;
@@ -133,15 +233,35 @@ public class XEasyPdfDocumentImager implements Serializable {
      * @return 返回pdf文档图像器
      */
     @SneakyThrows
-    public XEasyPdfDocumentImager image(OutputStream outputStream, XEasyPdfImageType imageType, int pageIndex) {
-        // 重置页面索引（0至文档总页面索引）
-        pageIndex = Math.min(Math.max(pageIndex, 0), this.document.getNumberOfPages() - 1);
+    public XEasyPdfDocumentImager image(OutputStream outputStream, XEasyPdfImageType imageType, int... pageIndex) {
         // 初始化pdfBox文档渲染器
         PDFRenderer renderer = new PDFRenderer(this.document);
-        // 渲染图片
-        BufferedImage bufferedImage = renderer.renderImageWithDPI(pageIndex, this.dpi);
-        // 写出图片
-        ImageIO.write(bufferedImage, imageType.name().toLowerCase(), outputStream);
+        // 设置渲染目的
+        renderer.setDefaultDestination(this.renderType.destination);
+        // 如果合并图片，则合并后保存图片
+        if (this.isMerge) {
+            // 定义图片列表
+            List<BufferedImage> imageList = new ArrayList<>(pageIndex.length);
+            // 遍历页面索引
+            for (int index : pageIndex) {
+                // 添加图片列表
+                imageList.add(renderer.renderImageWithDPI(Math.min(Math.max(index, 0), this.document.getNumberOfPages() - 1), this.dpi, this.getColorType()));
+            }
+            // 拼接图片
+            BufferedImage bufferedImage = XEasyPdfImageUtil.join(imageList, this.isHorizontalMerge);
+            // 写出图片
+            ImageIO.write(bufferedImage, imageType.name().toLowerCase(), outputStream);
+        }
+        // 否则单页保存图片
+        else {
+            // 遍历页面索引
+            for (int index : pageIndex) {
+                // 渲染图片
+                BufferedImage bufferedImage = renderer.renderImageWithDPI(Math.min(Math.max(index, 0), this.document.getNumberOfPages() - 1), this.dpi, this.getColorType());
+                // 写出图片
+                ImageIO.write(bufferedImage, imageType.name().toLowerCase(), outputStream);
+            }
+        }
         return this;
     }
 
@@ -152,5 +272,57 @@ public class XEasyPdfDocumentImager implements Serializable {
      */
     public XEasyPdfDocument finish() {
         return this.pdfDocument;
+    }
+
+    /**
+     * 渲染类型
+     */
+    public enum RenderType {
+        /**
+         * 导出
+         */
+        EXPORT(RenderDestination.EXPORT),
+        /**
+         * 显示
+         */
+        VIEW(RenderDestination.VIEW),
+        /**
+         * 打印
+         */
+        PRINT(RenderDestination.PRINT);
+
+        /**
+         * 目的
+         */
+        private final RenderDestination destination;
+
+        /**
+         * 有参构造
+         *
+         * @param destination 目的
+         */
+        RenderType(RenderDestination destination) {
+            this.destination = destination;
+        }
+    }
+
+    /**
+     * 获取颜色类型
+     *
+     * @return 返回颜色类型
+     */
+    private ImageType getColorType() {
+        // 如果透明，则返回ARGB类型
+        if (this.isAlpha) {
+            // 返回ARGB类型
+            return ImageType.ARGB;
+        }
+        // 如果灰度，则返回GRAY类型
+        if (this.isGray) {
+            // 返回GRAY类型
+            return ImageType.GRAY;
+        }
+        // 返回RGB类型
+        return ImageType.RGB;
     }
 }
